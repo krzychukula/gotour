6cfcc31c2dab0b59b0fbedf16b9c4a6e76707bb1
Krzysztof Kula <krzychukula@gmail.com>
2014-11-06 07:44:17 +0100

    Same

--- tree72.go
+++ tree72.go
@@ -25,6 +25,20 @@ func walkTree(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		if v1 != v2 || ok1 != ok2 {
+			fmt.Println(v1, v2, ok1, ok2)
+			return false
+		}
+		if !ok1 && !ok2 {
+			break
+		}
+	}
 	return true
 }
 
